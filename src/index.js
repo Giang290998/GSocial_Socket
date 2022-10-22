bb2b93501a833e5be546377dff5886a559bd44c7
Giang98 <nguyenhoanggiang290998@gmail.com>
2022-10-22 13:10:33 +0700

    fix cors 1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const { Server } = require("socket.io");
 let userOnlineArr = []
 let userOnlineNumber = 0
 const io = new Server(7000, {
+    cors: {
+        origin: "https://gsocial.onrender.com",
+        methods: ["GET", "POST"]
+    },
     maxHttpBufferSize: 2e8,
     pingTimeout: 40000
 })
